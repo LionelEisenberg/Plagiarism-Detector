30c0a3a0d08d64ec35b86950c6ca76cef67c003b
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2016-11-21 14:14:14 -0500

    Additions to Managing.cpp

--- src/Managing.cpp
+++ src/Managing.cpp
@@ -1,5 +1,10 @@
 #include "../include/Managing.hpp"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 int Managing::run() {
   cout << "run" <<endl;
@@ -8,4 +13,16 @@ int Managing::run() {
 
 void Managing::printSuspiciousPairs() {
   cout << "print" << endl;
+}
+
+vector<string> Managing::getMasterVector(string masterfilePath){
+  vector<string> fileList;
+  ifstream inFile;
+  inFile.open(masterfilePath);
+  string data;
+  while(getline(inFile,data)){
+    fileList.push_back(data);
+  }
+  inFile.close();
+  return fileList;
 }
