89531d4b16b80b28b0af44b8db3559be01850de3
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2016-11-21 13:35:43 -0500

    This is the header for the file that manages the input files, and passing them through our plagerism checking functions

---
+++ include/Managing.hpp
@@ -0,0 +1,25 @@
+/*                                                                                                                                          Lionel Eisenberg                                                                                                                            Sanat Deshpande                                                                                                                             
+
+This file manages the list of files to search, and feeds in                                                                                 appropriate input to the plagerism checker class                                                                                            */
+
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+
+class Managing{
+
+private:
+  vector<string> suspiciousFiles;
+  string masterfilePath;
+  char rigor;
+
+public:
+  vector<string> getMasterVector(string masterfilePath); //gets vector of list of files to test
+  string getContent(string filePath); //gets content of a file
+  void gotcha(string filePathOne, string filePathTwo); //increments suspicious files list
+  int run(); //runs all functions and interacts with plagerism checker functions
+
+
+};
