f2464a97b7f2a70e65d64c76af6d537545339c2c
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2016-11-30 20:39:20 -0500

    Added swap function to header of CheckPlagiarism

--- include/CheckPlagiarism.hpp
+++ include/CheckPlagiarism.hpp
@@ -24,3 +24,5 @@ int getLength(vector<string>::iterator begin, vector<string>::iterator end); //r
 bool isSameFile(vector<string>::iterator oneBegin, vector<string>::iterator oneEnd,vector<string>::iterator twoBegin,vector<string>::iterator twoEnd);
 bool checkControlC(vector<string>::iterator oneBegin, vector<string>::iterator oneEnd,vector<string>::iterator twoBegin,vector<string>::iterator twoEnd, char rigor); //checks two vectors for "Control-C" plagiarism using levenshtein distance method
 bool checkNgram(vector<string>::iterator oneBegin, vector<string>::iterator oneEnd,vector<string>::iterator twoBegin,vector<string>::iterator twoEnd, char rigor); //checks for "Control-C plagiarism using Ngram method
+
+void swap(vector<string>::iterator* oneBegin, vector<string>::iterator* oneEnd,vector<string>::iterator* twoBegin,vector<string>::iterator* twoEnd); //makes "one" iterators refer to "two" iterators
